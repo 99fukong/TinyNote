57676da72367ecf0b7858e8c5d0d27f7a8fcbfd6
99fukong <can6364200@163.com>
2023-03-13 23:59:24 +0800

    inital commit

--- houtai.py
+++ houtai.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify, render_template
 
 # app = Flask(__name__)
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='/root/git_rep/web/templates')
 
 @app.route('/')
 def index():
+    
+    # # 打开文件
+    # with open('/root/git_rep/web/templates/index.html', 'r', encoding='utf-8') as file:
+    #     # 读取文件内容
+    #     content = file.read()
+    # return content
+    
     return render_template('index.html')
 
 @app.route('/submit_diary', methods=['POST'])
@@ -34,4 +41,5 @@ def get_diaries():
     return jsonify(diaries)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    # host 改为 0.0.0.0 或者本机的公网IP，否则外网方问有问题
+    app.run(host="0.0.0.0", port=5050)
